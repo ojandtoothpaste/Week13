2ff0c6c2ceb15ea08c2a0d1c20052b71a4244677
Gabe Swenson <gswen75@icloud.com>
2024-02-28 09:46:55 -0700

    Update to login.jsx

--- login-app/src/Login.jsx
+++ login-app/src/Login.jsx
@@ -1,19 +1,11 @@
 import React, { useState } from "react";
 
-
-export const Login = (props) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email);
-    }
 //ANCHOR - login (h3), email and password input 
+export const Login = (props) => {
     return (
         <div className="auth-form-container">
             <h3>Log in</h3>
-            <form className="login-form" onSubmit={handleSubmit}>
+            <form className="login-form">
                 <label htmlFor="email">email</label>
                 <input type="email" placeholder="your email" id="email" name="email" className="text_area" />
                 <label htmlFor="password">password</label>
